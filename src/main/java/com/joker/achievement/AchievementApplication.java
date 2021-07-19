f48d2ab1400d642f5bd931a08a47bb65dee30c4e
mniu103 <mniu103@fiberhome.com>
2021-07-19 16:29:24 +0800

    base project

---
+++ src/main/java/com/joker/achievement/AchievementApplication.java
@@ -0,0 +1,17 @@
+package com.joker.achievement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Joker
+ */
+
+@SpringBootApplication
+public class AchievementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchievementApplication.class, args);
+    }
+
+}
